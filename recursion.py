d1fa4c9589faa6763cb5190ff034a21417fbdb6a
Srivarsha gowthu <103606595+Srivarshagowthu@users.noreply.github.com>
2024-01-26 23:35:15 +0530

    Add files via upload

--- recursion.py
+++ recursion.py
@@ -47,7 +47,7 @@ s=0
 su=3
 sub(0,arr,a,n,0,su)'''
 ##subset sivisions
-'''def sub(i,arr,a,n):
+def sub(i,arr,a,n):
     if i==n:
         print(arr)
         return
@@ -58,7 +58,7 @@ sub(0,arr,a,n,0,su)'''
 arr=[]
 a=[1,2,4,8]
 n=4
-sub(0,arr,a,n)'''
+sub(0,arr,a,n)
 ##bubble sort
 '''def bubbles(a,n):
     if n==1:
@@ -97,6 +97,65 @@ def merge(l,r):
     return result
 a=[10,20,2,3,4,6,8,56,89]
 print(merge_sort(a))'''
+##binary bits
+'''def binary(i,n,res):
+    if n==0:
+        print(res)
+        return
+    p=bin(i)[2::]
+    q=[]
+    q.append(p.count("1"))
+    res.append(*q.copy())
+    binary(i+1,n-1,res)
+res=[]
+print(binary(1,5,res))'''
+##COMBINATIONSL SUM
+#te index can be considered as many tines as possibke and
+#at end of each subset make sure to pop the lst and try to append new and
+#after that index go to ind+1)
+'''class Solution:
+    def combinationSum(self, arr: List[int], target: int) -> List[List[int]]:
+        def f1(i,arr,target,ds,res):
+            if i==len(arr):
+                if target==0:
+                    res.append(ds.copy())
+                return
+            if arr[i]<=target:
+                ds.append(arr[i])
+                f1(i,arr,target-arr[i],ds,res)
+                ds.pop()
+            f1(i+1,arr,target,ds,res)
+        ds=[]
+        res=[]
+        f1(0,arr,target,ds,res)
+        return res'''
+##combinationaal sum2
+#only for unique occurences
+#then we cn traverse from a new index to end every time
+#as they gve in sortig dolist sort() first
+'''class Solution:
+    def combinationSum2(self, arr: List[int], target: int) -> List[List[int]]:
+        def f1(ind,arr,target,res,ds):
+            if target==0:
+                res.append(ds.copy())
+                return
+            for i in range(ind,len(arr)):
+                if i>ind and arr[i]==arr[i-1]:
+                    continue
+                if arr[i]>target:
+                    break
+                ds.append(arr[i])
+                f1(i+1,arr,target-arr[i],res,ds)
+                ds.pop()
+        ds=[]
+        res=[]
+        p=sorted(arr)
+        f1(0,p,target,res,ds)
+        return res'''
+##
+
+
+        
 
         
 
